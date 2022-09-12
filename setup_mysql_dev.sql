b0d63940684df6f7807906578e400e4e34f0f201
bhankymyk <bankole123michael@gmail.com>
2022-09-12 11:35:37 +0100

    setup_mysql_dev.sql MODIFIED

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- Setting up MySql server for AirBnB_clone_v2
 
-CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON 'hbnb_dev_db' .* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON 'performance_schema' .* TO 'hbnb_dev'@'localhost';
-FLUSH PEIVILEGES;
+FLUSH PRIVILEGES;
